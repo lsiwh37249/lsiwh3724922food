67fcb69e3e788f40f0af2296ed8e209f73c7d7a7
lsiwh37249 <lsiwh37249@gmail.com>
2024-09-12 17:42:21 +0900

    dockerfile update

--- src/lsiwh3724922food/main.py
+++ src/lsiwh3724922food/main.py
@@ -2,8 +2,15 @@ from typing import Union
 from fastapi import FastAPI
 import pickle
 import time
+import os
+import pandas as pd
 
 app =FastAPI()
+
+def get_path():
+    file_dir = __file__
+    dir = os.path.dirname(file_dir)
+    return dir
 
 @app.get("/")
 def read_root():
@@ -13,6 +20,28 @@ def read_root():
 def food(name : str):
     # 시간을 구함
     # 음식 이름과 시간을 csv로 저장 -> /code/data/food.csv
-    my_time = time.strftime("%Y-%m-%d %H:%M:%S")
-    #"2024-09-14 11:12:13" 
-    return {"food" : name, "time" : f"{my_time}"}
+    formatted_time = time.strftime("%Y-%m-%d %H:%M:%S")
+    new_data = f"food : {name}, time : {formatted_time}"
+    
+    f_csv = os.path.join(get_path(), "code", "data")
+
+    if not os.path.exists(f_csv):
+        os.makedirs(f_csv)
+    #csv 파일 경로 설정
+    csv_file_path = os.path.join(f_csv, "food.csv")
+
+    # f_csv 폴더에 food.csv 파일이 없다면 생성 있다면 데이터를 추가
+    if not os.path.exists(csv_file_path):
+        df = pd.DataFrame(columns=["food", "time"])
+    else:
+        df = pd.read_csv(csv_file_path)
+
+    # 새 데이터 추가
+    new_data = pd.DataFrame([[name, formatted_time]], columns=["food", "time"])
+    df = pd.concat([df, new_data], ignore_index=True)
+
+    # CSV 파일 저장
+    df.to_csv(csv_file_path, index=False)
+
+    
+    return {"food" : name, "time" : f"{formatted_time}"}
