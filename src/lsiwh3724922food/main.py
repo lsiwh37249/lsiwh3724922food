c676685f1a43b09ddca74dfc2fcccd713984e481
lsiwh37249 <lsiwh37249@gmail.com>
2024-09-13 12:20:33 +0900

    CORS fix

--- src/lsiwh3724922food/main.py
+++ src/lsiwh3724922food/main.py
@@ -4,8 +4,24 @@ import pickle
 import time
 import os
 import pandas as pd
+from fastapi.middleware.cors import CORSMiddleware
 
 app =FastAPI()
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 def get_path():
     file_dir = __file__
